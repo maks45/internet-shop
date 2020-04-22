1cbc810b358cc5aaf131ae11c95dd856ec0c59dc
maks45 <maksim.durov45@gmail.com>
2020-04-22 19:30:16 +0300

    in Storage added Lists for buckets orders users

--- src/main/java/mate/academy/internetshop/db/Storage.java
+++ src/main/java/mate/academy/internetshop/db/Storage.java
@@ -2,14 +2,18 @@ package mate.academy.internetshop.db;
 
 import java.util.ArrayList;
 import java.util.List;
+import mate.academy.internetshop.model.Bucket;
 import mate.academy.internetshop.model.Item;
+import mate.academy.internetshop.model.Order;
+import mate.academy.internetshop.model.User;
 
 public class Storage {
     public static Long itemIdCounter = 0L;
     public static Long userIdCounter = 0L;
     public static Long orderIdCounter = 0L;
     public static Long bucketIdCounter = 0L;
-
     public static final List<Item> items = new ArrayList<>();
-
+    public static final List<Bucket> buckets = new ArrayList<>();
+    public static final List<Order> orders = new ArrayList<>();
+    public static final List<User> users = new ArrayList<>();
 }
