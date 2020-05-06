e01cb9645f34809afe7728c9740a28d5358790c9
maks45 <maksim.durov45@gmail.com>
2020-05-06 14:29:53 +0300

    added loger

--- src/main/java/mate/academy/internetshop/controllers/user/LoginController.java
+++ src/main/java/mate/academy/internetshop/controllers/user/LoginController.java
@@ -10,9 +10,11 @@ import mate.academy.internetshop.exceptions.AuthenticationException;
 import mate.academy.internetshop.lib.Injector;
 import mate.academy.internetshop.model.User;
 import mate.academy.internetshop.security.AuthenticationService;
+import org.apache.log4j.Logger;
 
 public class LoginController extends HttpServlet {
     private static final String USER_ID = "user_id";
+    private static final Logger LOGGER = Logger.getLogger(LoginController.class);
     private static final Injector INJECTOR = Injector.getInstance("mate.academy.internetshop");
     private final AuthenticationService authenticationService =
             (AuthenticationService) INJECTOR.getInstance(AuthenticationService.class);
@@ -34,8 +36,9 @@ public class LoginController extends HttpServlet {
             HttpSession session = req.getSession();
             session.setAttribute(USER_ID, user.getId());
         } catch (AuthenticationException e) {
+            LOGGER.warn("Authentication error : " + e);
             req.setAttribute("errorMsg", e.getMessage());
-            req.getRequestDispatcher("WEB-INF/views/users/login.jsp").forward(req, resp);
+            req.getRequestDispatcher("/WEB-INF/views/users/login.jsp").forward(req, resp);
             return;
         }
         req.getSession().setAttribute("user_id", user.getId());
