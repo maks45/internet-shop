d000ee92267b6bb8d492afa0f4d83ff68d2ea434
maks45 <maksim.durov45@gmail.com>
2020-05-12 11:15:11 +0300

    deleted comments from pom.xml

--- src/main/java/mate/academy/internetshop/dao/impl/ProductDaoImpl.java
+++ src/main/java/mate/academy/internetshop/dao/impl/ProductDaoImpl.java
@@ -8,7 +8,6 @@ import mate.academy.internetshop.db.Storage;
 import mate.academy.internetshop.model.Product;
 
 public class ProductDaoImpl implements ProductDao {
-
     @Override
     public Product create(Product product) {
         return Storage.addProduct(product);
@@ -25,8 +24,8 @@ public class ProductDaoImpl implements ProductDao {
     public Product update(Product product) {
         return Storage.products.set(
                 IntStream.range(0, Storage.products.size())
-                .filter(i -> Storage.products.get(i).getId().equals(product.getId()))
-                .findFirst().getAsInt(),
+                        .filter(i -> Storage.products.get(i).getId().equals(product.getId()))
+                        .findFirst().getAsInt(),
                 product);
     }
 
