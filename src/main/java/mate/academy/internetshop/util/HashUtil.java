44e34bbda68d014f13eb94ce79d3c3d9cab950f3
maks45 <maksim.durov45@gmail.com>
2020-05-18 21:25:19 +0300

    fixed codestyle

--- src/main/java/mate/academy/internetshop/util/HashUtil.java
+++ src/main/java/mate/academy/internetshop/util/HashUtil.java
@@ -8,20 +8,20 @@ import org.apache.log4j.Logger;
 public class HashUtil {
     private static final Logger LOGGER = Logger.getLogger(HashUtil.class);
 
-    public static byte[] getSalt(){
+    public static byte[] getSalt() {
         SecureRandom random = new SecureRandom();
         byte[] salt = new byte[16];
         random.nextBytes(salt);
         return salt;
     }
 
-    public static String hashPassword(String password, byte[] salt){
+    public static String hashPassword(String password, byte[] salt) {
         StringBuilder hashedPassword = new StringBuilder();
         try {
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
-            for(byte b: digest){
+            for (byte b: digest) {
                 hashedPassword.append(String.format("%02x",b));
             }
         } catch (NoSuchAlgorithmException e) {
